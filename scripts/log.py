c456892b7d68bceca1217b4a700bc688a7e46828
kakl <kakl@astro.cz>
2022-11-04 13:45:26 +0100

    multithread solution

--- scripts/log.py
+++ scripts/log.py
@@ -1,56 +1,64 @@
 #!/usr/bin/python3
 import time
+import threading
 import serial
 import logging
 import sys
 from logging.handlers import TimedRotatingFileHandler
-
-devices = ['/dev/ttyUSB0','/dev/ttyUSB1','/dev/ttyUSB2','/dev/ttyUSB3']
-
-baud = 9600
-#baud = 115200
-
+ 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-logname = "./resetitko.log"
-#logname = "./cardos.log"
+logname = 'log.txt'
 
 handler = TimedRotatingFileHandler(logname, when='h', interval=1, utc=True)
-#handler = TimedRotatingFileHandler(logname, when='s', interval=20, utc=True)
-#handler.setLevel(logging.INFO)
-#handler.suffix = "%Y%m%d%H%M"
 logger.addHandler(handler)
 
-'''
-# reset device
-ser.setDTR(False)
-time.sleep(0.5)
-ser.setDTR(True)
-time.sleep(0.5)
-ser.setDTR(False)
-'''
+
+def dolog(device, baudrate):
+    ser = serial.Serial(device, baudrate, timeout=20)
+
+    while True:
+        reading = ser.readline()
+        if (len(reading) > 0):
+            data = str(int(round(time.time(),2))) + ',' + str(reading[:-1],'utf-8')
+            print(data)
+            logger.info(data)
+
+
+devices = ['/dev/ttyUSB0','/dev/ttyUSB1','/dev/ttyUSB2','/dev/ttyUSB3']
+
+baudrates = [9600, 115200]
+
 success = False
 for port in devices:
-    try:
-        print("I am Trying",port)
-        ser = serial.Serial(port, baud, timeout=20)
-        time.sleep(10)
-        reading = ser.readline()
-        print(reading)
-        if ((str(reading,'utf-8')[0]=='$') or (str(reading,'utf-8')[0]=='#')):
-            success = True
-            break   
-    except:
-        pass
-        
+    for baud in baudrates:
+        try:
+            print("I am Trying", port, baud, "baud")
+            ser = serial.Serial(port, baud, timeout=20)
+            # reset device
+            ser.setDTR(False)
+            time.sleep(0.1)
+            ser.setDTR(True)
+            time.sleep(0.1)
+            ser.setDTR(False)
+            time.sleep(0.1)
+            ser.flushInput()
+            reading = ser.readline()
+            print(reading)
+            ser.close()
+            if (len(reading) == 0):
+                continue
+            if ((str(reading,'utf-8')[0]=='$') or (str(reading,'utf-8')[0]=='#')):
+                success = True
+                print('Starting thread', port, baud)
+                thread = threading.Thread(target=dolog, args=(port, baud, ))
+                thread.start()
+                break
+        except:
+            pass                    
 if (not success):
     sys.exit("I can not find any tty device.")
 
 while True:
-     reading = ser.readline()
-     if (len(reading) > 0):
-         data = str(int(round(time.time(),2))) + ',' + str(reading,'utf-8')
-         print(data)
-         logger.info(data)
-
+    None
